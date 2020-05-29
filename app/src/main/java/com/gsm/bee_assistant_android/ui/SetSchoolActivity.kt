396c32fce22957f4be6ed87125c4923175812da5
Im-Tae <dlaxo2420@naver.com>
2020-05-29 12:05:01 +0900

    setschoolactivity button event setting

--- app/src/main/java/com/gsm/bee_assistant_android/ui/SetSchoolActivity.kt
+++ app/src/main/java/com/gsm/bee_assistant_android/ui/SetSchoolActivity.kt
@@ -11,11 +11,10 @@ import com.gsm.bee_assistant_android.base.BaseActivity
 import com.gsm.bee_assistant_android.databinding.ActivitySetSchoolBinding
 import com.gsm.bee_assistant_android.ui.contract.SetSchoolContract
 import dagger.android.AndroidInjection
-import io.reactivex.Observable
 import kotlinx.android.synthetic.main.activity_set_school.*
 import javax.inject.Inject
 
-class SetSchoolActivity : BaseActivity(), SetSchoolContract.View, AdapterView.OnItemSelectedListener {
+class SetSchoolActivity : BaseActivity(), SetSchoolContract.View, AdapterView.OnItemSelectedListener, View.OnClickListener {
 
     @Inject
     override lateinit var presenter : SetSchoolContract.Presenter
@@ -40,20 +39,33 @@ class SetSchoolActivity : BaseActivity(), SetSchoolContract.View, AdapterView.On
     }
 
     override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-        Log.d("spinnerTest", school_Kind_Spinner.selectedItem.toString())
+        Log.d("spinnerTest", school_Kind_spinner.selectedItem.toString())
 
-        val schoolKind = school_Kind_Spinner.selectedItem.toString()
-        val region = region_Spinner.selectedItem.toString()
+        val schoolKind = school_Kind_spinner.selectedItem.toString()
+        val region = region_spinner.selectedItem.toString()
 
         presenter.getIdValue(schoolKind, region)
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) { }
 
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            R.id.setSchool_button -> {
+                // here
+                startActivity(MainActivity::class.java)
+            }
+            R.id.skip_button -> startActivity(MainActivity::class.java)
+        }
+    }
+
     override fun init() {
         presenter.getUserInfo()
-        school_Kind_Spinner.onItemSelectedListener = this
-        region_Spinner.onItemSelectedListener = this
+
+        school_Kind_spinner.onItemSelectedListener = this
+        region_spinner.onItemSelectedListener = this
+        setSchool_button.setOnClickListener(this)
+        skip_button.setOnClickListener(this)
     }
 
     override fun showKeyboard() {}
